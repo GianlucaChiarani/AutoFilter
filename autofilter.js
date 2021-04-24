86827ce7fe54ad7c37d05b7a67ddc707ebef37a0
GianlucaChiarani <26008534+GianlucaChiarani@users.noreply.github.com>
2021-04-24 13:07:52 +0200

    Update autofilter.js

--- autofilter.js
+++ autofilter.js
@@ -1,1 +1,91 @@
+/**
+ * AutoFilter (https://github.com/GianlucaChiarani/AutoFilter)
+ * @version 1.0
+ * @author Gianluca Chiarani
+ * @license The MIT License (MIT)
+ */
 
+(function ($) {
+    
+    $.autofilter = function( options ) {
+
+        var settings = $.extend({
+            showClass: 'show',
+            htmlAsTag: false,
+            subString: false,
+            caseSensitive: false,
+            animation: true,
+            duration: 0
+        }, options );
+
+        $('[data-filter]:not(input)').click(function() {
+            if (settings.htmlAsTag) {
+                var filterValue = $(this).html().trim();
+            } else {
+                var filterValue = $(this).attr('data-filter').trim();
+            }
+
+            if (filterValue!='')
+                af_filter(filterValue);
+            else
+                $('[data-tags],[data-to-filter]').fadeIn(settings.duration).addClass(settings.showClass);
+        });
+
+        $('input[data-filter]').keyup(function() {
+            var value = $(this).val();
+
+            if (value!='' && value.length>2) {
+                af_filter(value);
+            } else {
+                $('[data-tags],[data-to-filter]').fadeIn(settings.duration).addClass(settings.showClass);
+            }
+
+        });
+
+        function af_filter(filterValue) {
+            $('[data-tags],[data-to-filter]').each(function() {
+                var tags = $(this).attr('data-tags');
+                var tofilter = Array();
+                var valid = false;
+                
+                if (!settings.caseSensitive) 
+                    filterValue = filterValue.toLowerCase();
+
+                if (tags) {
+                    tofilter = tags.split(',');
+                } else {
+                    tofilter.push($(this).html());
+                    settings.subString = true;
+                }
+
+                if (!settings.caseSensitive) 
+                    tofilter = tofilter.map(v => v.toLowerCase());
+
+                if (settings.subString) {
+                    tofilter.forEach(function(toFilterOne) {
+                        if (toFilterOne.indexOf(filterValue) > -1) {
+                            valid = true;
+                        }
+                    });
+                } else {
+                    valid = tofilter.includes(filterValue);
+                }
+
+                if (valid) {
+                    if (settings.animation)
+                        $(this).fadeIn(settings.duration);
+                    
+                    $(this).addClass(settings.showClass);
+                } else {
+                    if (settings.animation)
+                        $(this).fadeOut(settings.duration)
+                    
+                    $(this).removeClass(settings.showClass);
+                }
+
+            });
+        }
+ 
+    };
+
+}(jQuery));
